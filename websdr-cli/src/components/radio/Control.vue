56ae63f9a6dc2d98459e78870e7672cdb4fd325c
Fabrice Crohas <fcrohas@gmail.com>
2017-08-14 10:11:01 +0200

    add vuex to store radio data

---
+++ websdr-cli/src/components/radio/Control.vue
@@ -0,0 +1,64 @@
+<template>
+  <md-card md-with-hover class="full-size">
+    <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">Radio</div>
+          <div class="md-subhead">Type</div>
+        </md-card-header-text>
+
+        <md-menu md-size="4" md-direction="bottom left">
+          <md-button class="md-icon-button" md-menu-trigger>
+            <md-icon>more_vert</md-icon>
+          </md-button>
+
+          <md-menu-content>
+            <md-menu-item>
+              <span>Move</span>
+              <md-icon>zoom_out_map</md-icon>
+            </md-menu-item>
+          </md-menu-content>
+        </md-menu>
+      </md-card-header>        
+    <md-card-media>
+        <p>Tuner frequency : {{centerFrequency}} <button @click="changeCenterFrequency(10000)">+</button> / <button @click="changeCenterFrequency(-10000)">-</button></p>
+        <p>Frequency : {{tunedFrequency}}</p>
+        <p>Bandwidth : {{currentBandwidth}}</p>
+    </md-card-media>
+
+    <md-card-actions>
+      <md-button class="md-icon-button">
+        <md-icon>play</md-icon>
+      </md-button>
+    </md-card-actions>
+  </md-card>      
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+
+  name: 'Control',
+  props: {
+  },
+  computed: mapGetters({
+    tunedFrequency: 'tunedFrequency',
+    currentBandwidth: 'currentBandwidth',
+    centerFrequency: 'centerFrequency'
+  }),
+  methods: mapActions({
+    changeCenterFrequency: 'changeCenterFrequency'
+  }),
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.full-size {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ websdr-cli/src/components/radio/Radio.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="full-height">
+		<md-layout md-row class="full-height">
+			<md-layout class="full-height" md-column>
+				<vue-draggable-resizable :parent="false" :resizable="true" :w="800" :h="400" :x="500" :y="500">
+					<waterfall></waterfall>
+				</vue-draggable-resizable>
+				<vue-draggable-resizable :parent="false" :resizable="true" :w="400" :h="300" :x="100" :y="100">
+					<control></control>
+				</vue-draggable-resizable>
+			</md-layout>
+		</md-layout>
+	</div>
+</template>
+
+<script>
+import Waterfall from './Waterfall'
+import Control from './Control'
+
+export default {
+  name: 'Radio',
+  components: { Waterfall, Control }
+}
+</script>
+
+<style lang="css" scoped>
+.full-height {
+	height: 100%;
+}
+</style>
