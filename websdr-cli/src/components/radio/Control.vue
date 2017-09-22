543b5b7c3034f92ee8abacb6fb2e22d2a0b57f9f
Fabrice Crohas <fabrice.crohas@itesoft.com>
2017-09-22 17:59:55 +0200

    Change fonts

--- websdr-cli/src/components/radio/Control.vue
+++ websdr-cli/src/components/radio/Control.vue
@@ -4,23 +4,29 @@
       <v-container fluid class="full-size">
         <v-layout row wrap class="display full-size">
           <v-flex xs12 class="full-size">
-            <span class="frequency">{{tunedFrequency}}<span class="tunerFrequency">Tuner frequency : {{centerFrequency}}</span></span>
-            <v-btn flat value="am" @click="changeCenterFrequency(-500000)">-</v-btn>
-            <v-btn flat value="am" @click="changeCenterFrequency(500000)">+</v-btn>
+            <v-flex xs11>
+              <span>
+                <span class="frequency">{{tunedFrequency | frequency}}</span>
+              </span>
+              <v-btn class="tuner-btn" flat small @click="changeCenterFrequency(-500000)">-</v-btn>
+              <v-btn class="tuner-btn" flat small @click="changeCenterFrequency(500000)">+</v-btn>
+            </v-flex>
+            <v-flex xs1>
+            </v-flex>
           </v-flex>
-          <v-flex xs12 class="full-size">
-            <v-btn flat value="am" @click="changeModulation('AM')">AM</v-btn>
-            <v-btn flat value="fm" @click="changeModulation('FM')">FM</v-btn>
-            <v-btn flat value="wfm" @click="changeModulation('WFM')">WFM</v-btn>
-            <v-btn flat value="usb" @click="changeModulation('USB')">USB</v-btn>
-            <v-btn flat value="lsb" @click="changeModulation('LSB')">LSB</v-btn>
+          <v-flex xs12 class="full-size tuner-group">
+            <v-btn class="tuner-btn" flat small value="am" @click="changeModulation('AM')">AM</v-btn>
+            <v-btn class="tuner-btn" flat small value="fm" @click="changeModulation('FM')">FM</v-btn>
+            <v-btn class="tuner-btn" flat small value="wfm" @click="changeModulation('WFM')">WFM</v-btn>
+            <v-btn class="tuner-btn" flat small value="usb" @click="changeModulation('USB')">USB</v-btn>
+            <v-btn class="tuner-btn" flat small value="lsb" @click="changeModulation('LSB')">LSB</v-btn>
           </v-flex>
-          <v-flex xs12 class="full-size">
-            <v-btn flat value="500" @click="changeFrequencyStep(500)">500 Hz</v-btn>
-            <v-btn flat value="1000" @click="changeFrequencyStep(1000)">1 Khz</v-btn>
-            <v-btn flat value="5000" @click="changeFrequencyStep(5000)">5 Khz</v-btn>
-            <v-btn flat value="12500" @click="changeFrequencyStep(12500)">12.5 Khz</v-btn>
-            <v-btn flat value="50000" @click="changeFrequencyStep(50000)">50 Khz</v-btn>
+          <v-flex xs12 class="full-size tuner-group">
+            <v-btn class="tuner-btn" flat small value="500" @click="changeFrequencyStep(500)">500 Hz</v-btn>
+            <v-btn class="tuner-btn" flat small value="1000" @click="changeFrequencyStep(1000)">1 Khz</v-btn>
+            <v-btn class="tuner-btn" flat small value="5000" @click="changeFrequencyStep(5000)">5 Khz</v-btn>
+            <v-btn class="tuner-btn" flat small value="12500" @click="changeFrequencyStep(12500)">12.5 Khz</v-btn>
+            <v-btn class="tuner-btn" flat small value="50000" @click="changeFrequencyStep(50000)">50 Khz</v-btn>
           </v-flex>
           <v-flex xs12 class="full-size">
             RF
@@ -77,6 +83,12 @@ export default {
       this.disconnected = true
     }
   },
+  filters: {
+    frequency: function (value) {
+      console.log(value)
+      return value.toString().replace(/(.{3})/g, '$1.')
+    }
+  },
   data () {
     return {
       modulation: 'fm',
@@ -99,16 +111,22 @@ export default {
 </script>
 
 <style lang="css" scoped>
+@font-face{
+ font-family:'digital-clock-font';
+ src: url('/static/fonts/digital-7 (mono).ttf');
+}
 .full-size {
   width: 100%;
   height: 100%;
   padding: 0px;
+  background: black;
 }
 .frequency {
-    color: black;
-    font-size: 54px;
-    font-weight: 2;
-    border: 2px solid lightgray;
+    font-family: 'digital-clock-font';
+    color: white;
+    font-size: 48px;
+    font-weight: 1;
+    border: 2px solid white;
 }
 .tunerFrequency {
     position: absolute;
@@ -117,4 +135,13 @@ export default {
     font-size: 8px;
     color: lightgray;
 }
+.tuner-btn {
+  border: white 1px solid;
+  color: white;
+  width: 50px;
+}
+.tuner-group {
+  border: white 1px solid;
+}
+
 </style>
